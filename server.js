8cfcc5c5c52ec1b89fe2d7a9dee50c81f81743d3
David-ben-Mesecke <chr.schiffer@gmail.com>
2020-01-20 11:06:20 +0100

    Commit 4

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Yeshiva World\n');
+  res.end('Hello Yeshiva Worlds\n');
 });
 
 server.listen(port, hostname, () => {
